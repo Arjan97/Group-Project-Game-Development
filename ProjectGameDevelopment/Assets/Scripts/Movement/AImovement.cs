b30d96147b51a14e67b886cf61ae034aab17aa2c
SamLeguijt <sam.leguijt@hva.nl>
2023-03-08 23:32:43 +0100

    Added comments to the AImovement script. Added script to the NPC GameObject, added the terrain to the script (ground plane). Tested for functuality: NPC moves towards a random location, but can be very far away (because the ground plane is enormous)

--- ProjectGameDevelopment/Assets/Scripts/Movement/AImovement.cs
+++ ProjectGameDevelopment/Assets/Scripts/Movement/AImovement.cs
@@ -5,43 +5,60 @@ using UnityEngine.AI;
 
 public class AImovement : MonoBehaviour
 {
+    /* Variable to access the NavMesh component of this GameObject */
     private NavMeshAgent agent;
 
+    /* Variable to get the Mesh of what this agent needs to move on. Made public to drag in the correct mesh in inspector */
     public MeshRenderer terrain;
 
+    /* Variable of type Vector3 that this agent will move towards */
     private Vector3 targetVector;
+
+    /* Variable to determine within what range the object needs to be before getting a new targetvector */
+    private float rangeOffset = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
+        /* Get access to the NavMeshAgent component of this object */
         agent = GetComponent<NavMeshAgent>();
+
+        /* Call NewDestination() to move towards a new destination on Start */
         NewDestination();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(transform.position, targetVector) <= 10)
+        /* Check to see if this object has reached its target */
+        if (Vector3.Distance(transform.position, targetVector) <= rangeOffset)
         {
-            Debug.Log("Get a new destination");
-
+            /* Call method to get a new destination upon reaching its destination */
             NewDestination();
         }
     }
 
+    /* Method to call to give this gameobject a new destination to move towards */
     private void NewDestination()
     {
+        /* Set the targetvector equal to a new random vector, gained by calling the GetNewWaypoint() method */
         targetVector = GetNewWaypoint();
 
+        /* Call the SetDestination method with the new targetvector as parameter to move towards the new vector*/
         agent.SetDestination(targetVector);
     }
+
+    /* Method that returns a random Vector, based on the X and Z min/max values of the terrain variable */
     private Vector3 GetNewWaypoint()
     {
-        float randomX = Random.Range(terrain.bounds.min.x, terrain.bounds.max.x);
-        float randomZ = Random.Range(terrain.bounds.min.z, terrain.bounds.max.z);
+        /* Get two random floats based on the terrain */
+        float randomX = Random.Range(terrain.bounds.min.x, terrain.bounds.max.x); //Get a random number between the Width of the terrain
+        float randomZ = Random.Range(terrain.bounds.min.z, terrain.bounds.max.z); //Get a random number between the depth of the terrain 
 
+        /* Make a new vector with the randomX and randomZ (Y set to 0 to prevent flying in the air) */
         Vector3 newTargetVector = new Vector3(randomX, 0, randomZ);
 
+        /* Return the new vector */
         return newTargetVector;
     }
 }
